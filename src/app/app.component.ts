29876de5a9c4f01839c5cc4ee6346e1a5423b2b6
unknown <sajidmulani2712@gmail.com>
2021-08-23 18:53:47 +0530

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'interpolation';
+
+  // Created an object for Interpolating the values by accesing key e.g detail.name // detail.city
+  detail = [{name: "sajid",city: "satara" },
+            {name: "abhijit",city: "pune" },
+            {name: "suraj",city: "chandrapur" }]
+
+            //assigned a and b its values to perform arithmatic operations on these two numbers 
+            //assigned to variables
+            a= 89;
+            b=45;
 }
